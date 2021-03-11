638c615bcdc36094674de6aa5df5fdc0c9851002
yushiro-div <61450307+yushiro-div@users.noreply.github.com>
2021-03-11 13:25:13 +0900

    222

--- src/List.js
+++ src/List.js
@@ -1,8 +1,14 @@
-export const List = ({title}) => {
+import React from 'react';
+
+
+export class List extends React.Component  {
+  render() {
+    const { title } = this.props;
   return (
     <div>
       <h4>リストです。</h4>
       <p>{title}</p>
     </div>
   )
+  }
 }
