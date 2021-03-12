8b069b3c7170f5d2364a047e8f1353254ce3c844
yushiro-div <61450307+yushiro-div@users.noreply.github.com>
2021-03-12 13:37:08 +0900

    10101010

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import { useState } from "react";
+import styled from 'styled-components'
+import { useState, useEffect } from "react";
 import { List } from "./List";
 import { Form } from "./Form";
-import { LANGUAGES } from "./const/languages" 
+import { getLanguages } from "./Comp/languages";
+
+
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  padding: 24px 64px 0;
+  border-bottom: 1px solid #E0E0E0;
+`
+
+const HeaderUl = styled.ul`
+  display: flex;
+  margin: 0;
+  padding: 0;
+`
+
+const HeaderLi = styled.li`
+  list-style: none;
+  padding: 4px 12px;
+  cursor: pointer;
+  border-bottom: ${props => props.focused ? '2px solid #F44336' : 'none'};
+`
+
+
 
 function App() {
   const [tab, setTab] = useState('list')
-  const [langs, setLangs] = useState(LANGUAGES)
+  const [langs, setLangs] = useState([])
+
+  useEffect(() => {
+    fetchLanguages();
+  }, [])
+
+  const fetchLanguages = async () => {
+    const languages = await getLanguages();
+    setLangs(languages);
+  }
+
 
   const addLang = (lang) => {
     setLangs([...langs, lang])
@@ -14,13 +49,12 @@ function App() {
 
   return (
     <div>
-      <header>
-        <ul>
-          <li onClick={() => setTab('list')}>リスト</li>
-          <li onClick={() => setTab('form')}>フォーム</li>
-        </ul>
-      </header>
-      <hr/>
+      <Header>
+        <HeaderUl>
+          <HeaderLi focused={tab === 'list'} onClick={() => setTab('list')}>リスト</HeaderLi>
+          <HeaderLi focused={tab === 'form'} onClick={() => setTab('form')}>フォーム</HeaderLi>
+        </HeaderUl>
+      </Header>
       {
         tab === 'list' ? <List langs={langs} />  :  <Form onAddLang={addLang}/>
       }
